09428a6b83b082b94e55940a0132b25e25958d1c
Mark-Apollo <magerton@rice.edu>
2017-06-11 19:42:33 -0500

    add REQUIRE, update docs, add spacing
    - adds REQUIRE file to package
    - updates docstrings
    - add spacing between operators for readability
    - more attention to types; allows for AbstractFloats instead of only
    Float64

--- example.jl
+++ example.jl
@@ -3,27 +3,29 @@ using Halton
 ## Halton Sequence
 
 # First 10 entries in Halton Sequence with base 3
-H = zeros(10)
+H = Vector{Float64}(10)
 base = 3
-HaltonSeq!(H,base)
+HaltonSeq!(H, base)
 
 # Skip first 100 entries
-HaltonSeq!(H,base, skip = 100)
+HaltonSeq!(H, base, skip=100)
 
 ## Halton Draws
 
 # 1000 Normal draws using a Halton sequence with base 17.
 # Default: Skip the first 500 draws
-H = zeros(1000)
+H = Vector{Float64}(1000)
 B = 17
-HaltonDraws!(H,B)
+HaltonDraws!(H, B)
+
 # Check your work:
-# using PyPlot
-# plt[:hist](sort!(HaltonDraws!(H,B)),100)
+using Plots
+gr()
+histogram(H)
 
 # 1000 Normal draws using a Halton sequence with base 17.
 ## Use Distributions.Uniform and skip no entries
 ## Requires Distributions.quantile(distr,x)
-H = zeros(1000)
+H = Vector{Float64}(1000)
 B = 17
-HaltonDraws!(H,B,distr=Distributions.Uniform(),skip=0)
+HaltonDraws!(H, B, distr=Distributions.Uniform(), skip=0)
