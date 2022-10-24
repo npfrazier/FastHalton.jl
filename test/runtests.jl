14c4b3c31132bfcddc19c80f9c465a1792718c5a
Mark A SFF <mjagerton@ucdavis.edu>
2022-10-23 20:23:44 -0700

    do sequence as an iterator

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,11 @@
 using Halton
 using Test
-
+# using HaltonSequences
 
 base2nums = [1//2 , 1//4 , 3//4 , 1//8 , 5//8 , 3//8 , 7//8 , 1//16, 9//16, 5//16]
 base3nums = [1//3, 2//3, 1//9, 4//9, 7//9, 2//9, 5//9, 8//9, 1//27, 10//27, ]
 
 n = length(base2nums)
 
-haltons = zeros(n)
-HaltonSeq!(haltons, 2, skip=0)
-@test float(base2nums) ≈ haltons
-HaltonSeq!(haltons, 3, skip=0)
-@test float(base3nums) ≈ haltons
+@test collect(HaltonSeq(2,0,10)) == base2nums
+@test collect(HaltonSeq(3,0,10)) == base3nums
