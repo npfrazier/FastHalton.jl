843d996d11fcb7e3403d33a794d27f454a86dda7
Mark A SFF <mjagerton@ucdavis.edu>
2022-10-23 17:12:14 -0700

    add base3 tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,12 @@ using Test
 
 
 base2nums = [1//2 , 1//4 , 3//4 , 1//8 , 5//8 , 3//8 , 7//8 , 1//16, 9//16, 5//16]
+base3nums = [1//3, 2//3, 1//9, 4//9, 7//9, 2//9, 5//9, 8//9, 1//27, 10//27, ]
 
 n = length(base2nums)
 
-base2test = zeros(n)
-HaltonSeq!(base2test, 2, skip=0)
-@test float(base2nums) == base2test
+haltons = zeros(n)
+HaltonSeq!(haltons, 2, skip=0)
+@test float(base2nums) ≈ haltons
+HaltonSeq!(haltons, 3, skip=0)
+@test float(base3nums) ≈ haltons
