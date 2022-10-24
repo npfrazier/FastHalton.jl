09b5a6d1d96b376cbfc45fe150749a807a101620
Mark A SFF <mjagerton@ucdavis.edu>
2022-10-23 17:10:24 -0700

    base2 tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,11 @@
 using Halton
-using Base.Test
+using Test
 
-# write your own tests here
-@test 1 == 2
+
+base2nums = [1//2 , 1//4 , 3//4 , 1//8 , 5//8 , 3//8 , 7//8 , 1//16, 9//16, 5//16]
+
+n = length(base2nums)
+
+base2test = zeros(n)
+HaltonSeq!(base2test, 2, skip=0)
+@test float(base2nums) == base2test
